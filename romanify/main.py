bbba333907bf6637a8b6ffa5db104be058de9138
Jane Turueva <tindomirel1994@gmail.com>
2019-02-12 20:42:35 +0300

    Fixed: #1

--- romanify/main.py
+++ romanify/main.py
@@ -18,7 +18,13 @@ mapping['IV'] = 4
 mapping['I'] = 1
 
 
-def arabic2roman(number) -> str:
+def arabic2roman(number: int) -> str:
+    if not isinstance(number, int):
+        raise TypeError('Please provide positive integer')
+
+    if number < 0:
+        raise ValueError('Please provide positive integer')
+
     result = ''
     for i,k in mapping.items():
         result += (i * (number // k))
@@ -26,8 +32,9 @@ def arabic2roman(number) -> str:
 
     return result
 
+
 def main():
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 2 or not sys.argv[1].isdigit():
         print('Please provide arabic number', file=sys.stderr)
         exit(1)
 
